50dcbbd05d029a6efbe32cc858363980c7605e3d
tbascoul <tbascoul+github@gmail.com>
2022-12-01 18:05:24 +0100

    solve day01_part2

--- day01/part2.py
+++ day01/part2.py
@@ -1,43 +1,24 @@
 from __future__ import annotations
 
 import argparse
+import heapq
 import os.path
-
-import pytest
 
 import support
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def compute(s: str) -> int:
-    n = 0
-    for i, c in enumerate(s.strip(), start=1):
-        if c == '(':
-            n += 1
-        elif c == ')':
-            n -= 1
-            if n == -1:
-                return i
-        else:
-            raise AssertionError(f'unexpected: {c!r}')
-    raise AssertionError('unreachable')
-
-
-INPUT_S = '''\
-()())
-'''
-EXPECTED = 5
-
-
-@pytest.mark.parametrize(
-    ('input_s', 'expected'),
-    (
-        (INPUT_S, EXPECTED),
-    ),
-)
-def test(input_s: str, expected: int) -> None:
-    assert compute(input_s) == expected
+def compute(s: str, top_n: int) -> int:
+    current_sum = 0
+    max_heap = []
+    for line in s.splitlines(): 
+        try:
+            current_sum += int(line)
+        except ValueError:
+            heapq.heappush(max_heap, current_sum * (-1))
+            current_sum = 0
+    return sum([heapq.heappop(max_heap) for _ in range(top_n)]) * (-1)
 
 
 def main() -> int:
@@ -46,7 +27,7 @@ def main() -> int:
     args = parser.parse_args()
 
     with open(args.data_file) as f, support.timing():
-        print(compute(f.read()))
+        print(compute(f.read(), 3))
 
     return 0
 
